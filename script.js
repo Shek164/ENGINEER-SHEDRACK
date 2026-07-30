ef51e278de7adf0c96a6881bda6107ff0c99d155
Shedrack Waema <291545540+Shek164@users.noreply.github.com>
2026-07-31 01:04:35 +0300

    Remove animated counters implementation
    Removed animated counters functionality from script.js.

--- script.js
+++ script.js
@@ -147,47 +147,6 @@ if (footer) {
 }
 
 console.log("Portfolio Loaded Successfully");
-/* Animated Counters */
-
-const counters = document.querySelectorAll(".counter");
-
-const counterObserver = new IntersectionObserver(entries => {
-
-entries.forEach(entry => {
-
-if(entry.isIntersecting){
-
-const counter = entry.target;
-
-const target = +counter.dataset.target;
-
-let count = 0;
-
-const update = () => {
-
-count += target / 100;
-
-if(count < target){
-
-counter.innerText = Math.floor(count);
-
-requestAnimationFrame(update);
-
-}else{
-
-counter.innerText = target;
-
-}
-
-};
-
-update();
-
-counterObserver.unobserve(counter);
-
-}
-
-});
 
 });
 
